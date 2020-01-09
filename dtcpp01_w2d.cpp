08e75de23f1be7f4baf7f241cad627dfee1040ae
cloudostrich <cloudostrich@gmail.com>
2020-01-09 16:44:50 +0800

    w2d full spec

--- dtcpp01_w2d.cpp
+++ dtcpp01_w2d.cpp
@@ -15,8 +15,6 @@
 #include "DTCProtocol.cpp"
 #include "DTCProtocol.h"
 
-//using namespace std;
-
 // Working Flag
 static bool fin = false;
 const int HRTBTINTERVAL= 15;
@@ -27,9 +25,9 @@ struct Header{
 	};
 
 void send_heartbeat(int &sock) {
-	// Create heartbeat message
+	/* Create and send heartbeat message */
 	std::cout << "HeartBeater Started...\n";
-	char buf[16];
+	char buf[16]; // heartbeat msg is only 16 b
 	DTC::s_Heartbeat hrtbt;
 	memcpy(buf, &hrtbt, hrtbt.Size);
 	while (!fin) {
@@ -37,7 +35,6 @@ void send_heartbeat(int &sock) {
 		if (sendRes == -1) {
 			std::cout << "Could not send Heartbeat to Server!!!\n";
 		}
-		//std::cout << "Heartbeated...\n";
 		std::this_thread::sleep_for(std::chrono::seconds(HRTBTINTERVAL));
 	}
 }
@@ -55,15 +52,18 @@ void send_message(int sock, void *msg2send, u_int16_t size){
 }
 
 void listen_server(int &sock){
-	// Listen for messages from dtc server
+	/* Listen for messages from dtc server */
 	std::cout << "listen_server thread started! \n";
-	std::fstream pricefile;
-	pricefile.open("pricefile.csv", std::ios::app);
+	std::fstream fileprice, filelog;
+	fileprice.open("fileprice.csv", std::ios::app);
+	filelog.open("filelog", std::ios::app);
 	char buf[512];
 	Header header;
 	DTC::s_EncodingResponse enc_resp;
 	DTC::s_LogonResponse logon_resp;
 	DTC::s_MarketDataUpdateBidAskCompact mkt_bac;
+	DTC::s_MarketDataUpdateTradeCompact mkt_tc;
+	DTC::s_MarketDataSnapshot mkt_snap;
 
 	while (!fin) {
 		memset(buf, 0, 128);
@@ -78,36 +78,79 @@ void listen_server(int &sock){
 			time_t now1 = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 			auto now = std::put_time(std::localtime(&now1), "%F %T"); 
 			if (header.type != 3) {	// ignore heartbeat responses
-				// switch case here
 				switch(header.type) 
 				{
 					case 117 :  // Market Data Udate BidAsk Compact
 						{
 						//mkt_bac.Clear();
 						mkt_bac.CopyFrom(static_cast<void*>(buf));
-						pricefile << now << ", " << "117, " <<
+						fileprice << now << ", 117, " <<
 							mkt_bac.GetMessageSize() << ", " <<
 							mkt_bac.GetDateTime() << ", " << 
 							mkt_bac.GetSymbolID() << ", " << 
 							mkt_bac.GetBidPrice() << ", " << 
 							mkt_bac.GetBidQuantity() << ", " << 
 							mkt_bac.GetAskPrice() << ", " << 
-							mkt_bac.GetAskQuantity() << ", " << "\n";
-						std::cout << now  << ", " << "117, " <<
+							mkt_bac.GetAskQuantity() << "\n";
+						std::cout << now  << ", 117, " <<
 							mkt_bac.GetSymbolID() << ", " << 
 							mkt_bac.GetBidPrice() << ", " << 
-							mkt_bac.GetAskPrice() << ", " << "\n";
+							mkt_bac.GetAskPrice() << "\n";
 						break;
 						}
-					case 112 :
-						std::cout << "MARKET_DATA_UPDATE_TRADE_COMPACT: " << header.type <<"\n";
+					case 112 :	// Market Data Update Trade Compact
+						{
+						mkt_tc.CopyFrom(static_cast<void*>(buf));
+						fileprice << now << ", 112, " <<
+							mkt_tc.GetMessageSize() << ", " <<
+							mkt_tc.GetDateTime() << ", " << 
+							mkt_tc.GetSymbolID() << ", " << 
+							mkt_tc.GetPrice() << ", " << 
+							mkt_tc.GetVolume() << ", " << 
+							mkt_tc.GetAtBidOrAsk() << "\n";
+						std::cout << now << ", 112, " <<
+							mkt_tc.GetSymbolID() << ", " << 
+							mkt_tc.GetPrice() << ", " << 
+							mkt_tc.GetVolume() << ", " << 
+							//mkt_tc.GetAtBidOrAsk() << "\n";
 						break;
-					case 104 :
-						std::cout << "MARKET_DATA_SNAPSHOT: " << header.type <<"\n";
+						}
+					case 104 :	// Market Data Snapshot
+						{
+						mkt_snap.CopyFrom(static_cast<void*>(buf));
+						filelog << now << ", 104, " <<
+							mkt_snap.GetMessageSize() << ", " <<
+							mkt_snap.GetSymbolID() << ", " << 
+							mkt_snap.GetSessionSettlementPrice() << ", " << 
+							mkt_snap.GetSessionOpenPrice() << ", " << 
+							mkt_snap.GetSessionHighPrice() << ", " << 
+							mkt_snap.GetSessionLowPrice() << ", " <<
+							mkt_snap.GetSessionVolume() << ", " <<
+							mkt_snap.GetSessionNumTrades() << ", " <<
+							mkt_snap.GetOpenInterest() << ", " <<
+							mkt_snap.GetBidPrice() << ", " <<
+							mkt_snap.GetAskPrice() << ", " <<
+							mkt_snap.GetAskQuantity() << ", " <<
+							mkt_snap.GetBidQuantity() << ", " <<
+							mkt_snap.GetLastTradePrice() << ", " <<
+							mkt_snap.GetLastTradeVolume() << ", " <<
+							mkt_snap.GetLastTradeDateTime() << ", " <<
+							mkt_snap.GetBidAskDateTime() << ", " <<
+							mkt_snap.GetSessionSettlementDateTime() << ", " <<
+							mkt_snap.GetTradingSessionDate() << ", " <<
+							mkt_snap.GetTradingStatus() << ", " <<
+							mkt_snap.GetMarketDepthUpdateDateTime() << ", " << "\n";
+						std::cout << "MARKET_DATA_SNAPSHOT: 104 " <<"\n";
 						break;
+						}
 					case 7 : // Encoding Response
 						{ 
 						enc_resp.CopyFrom(static_cast<void*>(buf));
+						filelog << now << ", 7, " <<
+							enc_resp.GetMessageSize() << ", " <<
+							enc_resp.GetProtocolVersion() << ", " <<
+							enc_resp.GetEncoding() << ", " <<
+							enc_resp.GetProtocolType() << "\n";
 						std::cout << "ENCODING_RESPONSE: " <<
 							enc_resp.GetMessageSize() << ", " <<
 							enc_resp.GetProtocolVersion() << ", " <<
@@ -118,6 +161,28 @@ void listen_server(int &sock){
 					case 2 : //DTC::s_LogonResponse logon_resp;
 						{
 						logon_resp.CopyFrom(static_cast<void*>(buf));
+						filelog << now << ", 2, " <<
+							logon_resp.GetMessageSize() << ", "  << 
+							logon_resp.GetProtocolVersion() << ", " << 
+							logon_resp.GetResult() << ", " <<
+							logon_resp.GetResultText() << ", " <<
+							logon_resp.GetReconnectAddress() << ", " << 
+							logon_resp.GetInteger_1() << ", " << 
+							logon_resp.GetServerName() << ", " << 
+							logon_resp.GetMarketDepthUpdatesBestBidAndAsk() << ", " << 
+							logon_resp.GetTradingIsSupported() << ", " << 
+							logon_resp.GetOCOOrdersSupported() << ", " << 
+							logon_resp.GetOrderCancelReplaceSupported() << ", " << 
+							logon_resp.GetSymbolExchangeDelimiter() << ", " << 
+							logon_resp.GetSecurityDefinitionsSupported() << ", " << 
+							logon_resp.GetHistoricalPriceDataSupported() << ", " <<
+							logon_resp.GetResubscribeWhenMarketDataFeedAvailable() <<  ", " << 
+							logon_resp.GetMarketDepthIsSupported() << ", " << 
+							logon_resp.GetOneHistoricalPriceDataRequestPerConnection() << ", " << 
+							logon_resp.GetBracketOrdersSupported() << ", " << 
+							logon_resp.GetUseIntegerPriceOrderMessages() << ", " << 
+							logon_resp.GetUsesMultiplePositionsPerSymbolAndTradeAccount() << ", " << 
+							logon_resp.GetMarketDataSupported() << "\n";
 						std::cout << "LOGON_RESPONSE: " << 
 							logon_resp.GetMessageSize() << ", "  << 
 							logon_resp.GetProtocolVersion() << ", " << 
@@ -142,13 +207,18 @@ void listen_server(int &sock){
 							logon_resp.GetMarketDataSupported() << "\n";
 						break;
 						}
-					default:
+					default:	// Catch all messages not covered yet
+						{
+						filelog << now << "Unknown: " << header.type << "\n";
 						std::cout << "Unknown: " << header.type << "\n";
+						}
 				}
 			}
 		}
 	}
-	pricefile.close()
+	fileprice.close();
+	filelog.close();
+	std::cout " Closed 2 files...\n";
 }
 
 
