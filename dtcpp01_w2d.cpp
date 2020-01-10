c751585d5eeb0d9c2f7951f64b98f3262a3965d2
cloudostrich <cloudostrich@gmail.com>
2020-01-10 18:30:51 +0800

    latest w2d ver

--- dtcpp01_w2d.cpp
+++ dtcpp01_w2d.cpp
@@ -1,3 +1,20 @@
+/*
+using this block as example to add mutliple symbols
+	const char sym1[10] = "abcdef";
+	const char sym2[10] = "oilngas";
+	const char sym3[10] = "rubbers";
+
+	const char *symlist[10];
+
+	symlist[0] = sym1;
+	symlist[1] = sym2;
+	symlist[2] = sym3;
+	symlist[3] = NULL;
+
+	for (int i=0; symlist[i] != NULL; i++) 
+		printf("%s\n", symlist[i]);
+		*/
+
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
